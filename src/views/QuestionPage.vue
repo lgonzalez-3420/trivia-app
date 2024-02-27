97a739895d5d6d812984eed709ac2733d70f3518
lgonzalez-3420 <little.red.cloud@gmail.com>
2024-02-26 18:15:34 -0600

    Fixed Question

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
-
+import BaseTitle from '@/components/BaseTitle.vue';
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
@@ -11,10 +11,17 @@ onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
 })
 
-
 </script>
 
 
 <template>
-{{ question }}
+
+<div v-if="question" class="">
+  <BaseTitle>{{ question.category }}</BaseTitle>
+  {{ question.question }}
+</div>
+<div v-else class="">
+  Loading...
+</div>
+
 </template>
